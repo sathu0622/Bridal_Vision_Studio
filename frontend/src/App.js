77f2dc693ce97b66b1c52ce92b87019f481e5a45
SGeerthan <it22346568@my.sliit.lk>
2024-09-20 07:59:08 +0530

    flask added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProduct from './productManagment/AddProduct';
 import ViewProduct from './productManagment/ViewProduct';
 import AdminView from './productManagment/AdminView';
+import FrontPage from './pages/FrontPage'
+
 import '../src/styles.css';
+
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Welcome to the Product Management App</h1>} />
+        <Route path="/" element={<FrontPage/>} />
         <Route path="/add-product" element={<AddProduct />} />
         <Route path="/view-product" element={<ViewProduct />} />
         <Route path="/admin-product" element={<AdminView />} />
-
       </Routes>
     </Router>
   );
